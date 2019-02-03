ac1db7e9fac3c4076064ca0b57e4d9c9b82091bf
Edward\Drawde <efrojasm@gmail.com>
2019-02-03 11:33:14 -0600

    Workshops

---
+++ ApiWorkshop/ApiWorkshop/Controllers/StudentController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiWorkshop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWorkshop.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Students")]
+    public class StudentController : Controller
+    {
+        private readonly StudentDBContext context;
+
+        public StudentController(StudentDBContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/Students
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return context.Students.ToList();
+        }
+
+        [HttpGet("{id}", Name ="Student Created")]
+        public IActionResult GetById(int id)
+        {
+            var student = context.Students.FirstOrDefault(x => x.Id == id);
+
+            if(student==null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            if(ModelState.IsValid)
+            {
+                context.Students.Add(student);
+                context.SaveChanges();
+                return new CreatedAtRouteResult("Student Created", new { id = student.Id }, student);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Student student, int id)
+        {
+            if (student.Id!=id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(student).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var student = context.Students.FirstOrDefault(x => x.Id == id);
+
+            if (student==null)
+            {
+                return NotFound();
+            }
+
+            context.Students.Remove(student);
+            context.SaveChanges();
+            return Ok(student);
+        }
+
+
+    }
+}
---
+++ ApiWorkshop/ApiWorkshop/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiWorkshop.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ApiWorkshop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<StudentDBContext>(options =>
+            options.UseInMemoryDatabase("StudenDB"));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, StudentDBContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            if (!context.Students.Any())
+            {
+                context.Students.AddRange(new List<Student>()
+
+                {
+                    new Student() {FirstName="Edward",LastName="Rojas",Email="efrojasm@gmail.com",Address="Aguas Zarcas" },
+                    new Student() { FirstName = "Francisco", LastName = "Mendez", Email = "efrojasm23@gmail.com", Address = "Los Chiles" }
+
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
